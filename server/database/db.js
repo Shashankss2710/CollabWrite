5e7072f78ddedcbc9630938b5968a0c475cbbb89
Shashank <shashanksinghss872@gmail.com>
2023-10-14 19:04:38 +0530

    feat: update UI and BE

--- server/database/db.js
+++ server/database/db.js
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
 
 const Connection = async (username="user1", password="27102001")=>{
-    const url="mongodb+srv://${username}:${password}@collabwrite.rfhw2gv.mongodb.net/?retryWrites=true&w=majority";
+    const url="mongodb+srv://${username}:${password}@CollabWrite.rfhw2gv.mongodb.net/?retryWrites=true&w=majority";
 }
 
 
 try{
-  await mongoose.connect("mongodb+srv://user1:27102001@collabwrite.rfhw2gv.mongodb.net/?retryWrites=true&w=majority",{useUnifiedTopology:true,useNewUrlParser:true});
+  await mongoose.connect("mongodb+srv://user1:27102001@CollabWrite.rfhw2gv.mongodb.net/?retryWrites=true&w=majority",{useUnifiedTopology:true,useNewUrlParser:true});
    console.log('database connected successfully');
 }catch(error){
     console.log('error while connecting to database',error);
